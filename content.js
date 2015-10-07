ff9ccd9160de07091b5028071e358e1e5743093c
1000ch <shogo.sensui@gmail.com>
2015-10-07 23:48:09 +0900

    debounce event

--- content.js
+++ content.js
@@ -1,6 +1,45 @@
 (function () {
-  document.addEventListener('DOMNodeInserted', e => {
-    'use strict';
+
+  'use strict';
+
+  function debounce(func, wait) {
+
+    let timeout;
+    let args;
+    let context;
+    let timestamp;
+    let result;
+
+    const later = function() {
+
+      let last = Date.now() - timestamp;
+
+      if (last < wait && last >= 0) {
+        timeout = setTimeout(later, wait - last);
+      } else {
+        timeout = null;
+        result = func.apply(context, args);
+        if (!timeout) {
+          context = args = null;
+        }
+      }
+    };
+
+    return function() {
+
+      context = this;
+      args = arguments;
+      timestamp = Date.now();
+
+      if (!timeout) {
+        timeout = setTimeout(later, wait);
+      }
+
+      return result;
+    };
+  }
+
+  document.addEventListener('DOMNodeInserted', debounce(e => {
 
     let elements = document.getElementsByClassName('rg_di');
 
@@ -24,5 +63,5 @@
         image.src = matches[1];
       }
     }
-  });
+  }), 100);
 })();
